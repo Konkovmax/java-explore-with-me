b532737439946eb40cf0e213dde1e67bdff96953
Konkovmax <konkovmax@yandex.ru>
2023-01-10 06:40:26 +0300

    clear before PR

--- ewm-service/src/main/java/ru/practicum/ewmservice/compilations/CompilationRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.ewmservice.compilations;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompilationRepository extends JpaRepository<Compilation, Integer> {
-
-}
