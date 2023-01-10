8feedc6674cb9277f488431b8a40f3caaabd5e9f
Konkovmax <konkovmax@yandex.ru>
2023-01-10 06:19:42 +0300

    after review 1

---
+++ ewm-service/src/main/java/ru/practicum/ewmservice/compilation/NewCompilationDto.java
@@ -0,0 +1,21 @@
+package ru.practicum.ewmservice.compilation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class NewCompilationDto {
+    private int id;
+    @NotEmpty
+    private String title;
+    private boolean pinned;
+    private Set<Integer> events = new HashSet<>();
+}
